846437664cd30c3ff6629a4984bbf9ce8d178772
Kiyanoosh Kabootaran <kabootaran1384@gmail.com>
2026-09-03 13:09:03 +0330

    Add entity framework sqlite

--- GameStore.Api/obj/Debug/net10.0/GameSore.Api.AssemblyInfo.cs
+++ GameStore.Api/obj/Debug/net10.0/GameSore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameSore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f523550f254c9c68a50260d0d477dcb68b2eb4a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8adb8f37a0c569cfe90ad3359ae2d2b3744c14fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameSore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameSore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
